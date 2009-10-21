52b788103eef3712dff5e16dcd064600a144c88b
R.Bharathan <bharathan100@gmail.com>
2009-10-21 23:31:00 +0530

    I'm trying to write a basic ray tracer using orthographic projection first

---
+++ save_image.py
@@ -0,0 +1,19 @@
+import Image
+
+def save_bmp(name, buf, image_size):
+    image = Image.new("RGB", image_size)
+    for i in range(image_size[0]):
+        for j in range(image_size[1]):
+            image.putpixel((i,j), buf[i * image_size[1] + j])
+    image.save(name + ".bmp")
+
+if __name__ == "__main__":
+    #Test code
+    buf = []
+    i = 0
+        
+    while (i < 1024*768):
+        buf.append((0, 255, 0))
+        i += 1
+    
+    save_bmp("test", buf)
